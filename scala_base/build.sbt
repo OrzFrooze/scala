26b0ce535eafbbf9c1c13aee404bd28525af4fa5
OrzFrooze <23569076+OrzFrooze@users.noreply.github.com>
2021-02-25 21:17:22 +0800

    modify

--- scala_base/build.sbt
+++ scala_base/build.sbt
@@ -2,4 +2,4 @@ name := "scala_base"
 
 version := "0.1"
 
-scalaVersion := "2.13.5"
+//scalaVersion := "2.13.5"
--- scala_base/src/main/scala/unit1/Test.scala
+++ scala_base/src/main/scala/unit1/Test.scala
@@ -1,43 +1,43 @@
 package unit1
 
-object Test{
-    def max (values:Int*):Int={
-      values(0)
-    }
+object Test {
+  def max(values: Int*): Int = {
+    values(0)
+  }
 
-    def list():Unit = {
-      var list = List()
-      
-    }
-    def getInfo() ={
-      ("aaa","bbb","cccc")
-    }
-    
-def mail(mailClass:String="first",des:String="s"):Unit = {
+  def list(): Unit = {
+    var list = List()
+
+  }
+  def getInfo() = {
+    ("aaa", "bbb", "cccc")
+  }
+
+  def mail(mailClass: String = "first", des: String = "s"): Unit = {
     println(s" $mailClass $des")
-}
-    def javaArray():Unit ={
-      val a:Int = 10
-      val list = new java.util.ArrayList[String]
-      list.ensureCapacity(a)
-      list.add("sfsf")
-      list.add("222")
-      list.add("232")
-      list.forEach(println)
-    }
-    def ls() = println("=============")
-    def main(args: Array[String]): Unit = {
-        val value = List(1,2,3,4,5)
-        value.map(println)
-        val name = "teshi"
-        println(s"hello world $name")
-        for(i <- 1 until 3) println(s"$i c")
-        println("-------------")
-        (1 to 3).foreach(println)
-        println("-------")
-        javaArray()
-        ls
-        val (a,b,c) = getInfo()
-        println(s"$a $b $c")
-    }
+  }
+  def javaArray(): Unit = {
+    val a: Int = 10
+    val list = new java.util.ArrayList[String]
+    list.ensureCapacity(a)
+    list.add("sfsf")
+    list.add("222")
+    list.add("232")
+    list.forEach(println)
+  }
+  def ls() = println("=============")
+  def main(args: Array[String]): Unit = {
+    val value = List(1, 2, 3, 4, 5)
+    value.map(println)
+    val name = "teshi"
+    println(s"hello world $name")
+    for (i <- 1 until 3) println(s"$i c")
+    println("-------------")
+    (1 to 3).foreach(println)
+    println("-------")
+    javaArray()
+    ls
+    val (a, b, c) = getInfo()
+    println(s"$a $b $c")
+  }
 }
